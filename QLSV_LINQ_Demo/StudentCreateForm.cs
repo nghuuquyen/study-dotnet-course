cbe6cc80bbec99d39d653fb8ec64b5f82264d4e5
Nguyen Huu Quyen <studyit.vn@gmail.com>
2017-04-03 09:49:39 +0700

    add comments

--- QLSV_LINQ_Demo/StudentCreateForm.cs
+++ QLSV_LINQ_Demo/StudentCreateForm.cs
@@ -22,10 +22,15 @@ namespace QLSV_LINQ_Demo
 
         private void initLopComboBox()
         {
+            // Chú ý là ở đây tạo ra một thuộc tính mới có tên là "SF"
             var lops = from lop in db.Lops select new { lop.Ten_Lop, lop.ID_Lop , SF = lop.ID_Lop + "-" + lop.Ten_Lop };
 
             cbxLop.DataSource = lops;
+            // Sau đó gán thuộc tính hiển thị là "SF"
             cbxLop.DisplayMember = "SF";
+
+            // Và thuộc tính sẽ lấy giá trị chính là "ID_Lop"
+            cbxLop.ValueMember = "ID_Lop";
         }
 
         private void btnThoat_Click(object sender, EventArgs e)
@@ -43,11 +48,15 @@ namespace QLSV_LINQ_Demo
                     Name = txtHoTen.Text,
                     ID_Lop = int.Parse(cbxLop.SelectedValue.ToString())
                 };
-
+                // Lưu tạm nó vào SVs hiện tại , chưa được lưu vào DB
                 db.SVs.InsertOnSubmit(s);
-            }finally
+            }
+            finally
             {
                 MessageBox.Show("Saving Data.");
+
+                // Tới đây mới thực sự lưu vào DB, như vậy có thể chèn nhiều Record cùng 
+                // lúc.
                 db.SubmitChanges();
             }
 
