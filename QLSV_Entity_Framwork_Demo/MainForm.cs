96a83215122cafcc1534b59d715dd901ffe7d1e0
Nguyen Huu Quyen <studyit.vn@gmail.com>
2017-04-10 09:55:31 +0700

    Demo Entity Framwork

---
+++ QLSV_Entity_Framwork_Demo/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLSV_Entity_Framwork_Demo
+{
+    public partial class MainForm : Form
+    {
+        QLSVEntities db = new QLSVEntities();
+        public MainForm()
+        {
+            InitializeComponent();
+
+            dataGridView1.DataSource = db.SVs.Select(s => s).ToList();
+        }
+    }
+}
